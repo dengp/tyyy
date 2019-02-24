947f0c944ecc5b99fd8e38d6c0af6f5da15576de
dengping <ping.deng@kuwo.cn>
2019-02-24 20:14:21 +0800

    feat:加入我们

--- src/pages/join/App.vue
+++ src/pages/join/App.vue
@@ -4,6 +4,7 @@
     <div class="con">
       <div class="titleBox">
         <h1>加入我们</h1>
+        <img src="../../assets/images/join/titlePicBig.png" />
       </div>
       <div class="conWrap">
         <div class="departmentList">
@@ -441,38 +442,149 @@ export default {
 <style lang="less" scoped>
 .main {
   width: 100%;
+  color:rgba(51,51,51,1);
+  .con {
+    background: #f0f0f0;
+    padding-bottom: 50px;
+    .titleBox {
+      width:100%;
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;  
+      img {
+        width: 1440px;
+        height: 180px;
+      }
+      h1 {
+        position: absolute;
+        font-size:42px;
+        font-weight:400;
+        line-height:59px;
+      }
+    }
+    .conWrap {
+      position: relative;
+      margin: -20px 15.28% 0;
+      background: #ffffff;
+      box-shadow:0px 0px 20px 0px rgba(0,0,0,0.05);
+      border-radius:6px;
+      padding: 0 30px;
+      .departmentList {
+        .departmentItem {
+          .departmentItemTitle {
+            padding: 0 20px 0 10px;
+            height: 80px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 1px solid #e5e5e5;
+            .departmentItemName {
+              font-size: 24px;
+              font-weight: 500;
+            }
+            .icon_close {
+              width: 36px;
+              height: 36px;
+              background: url("~assets/images/join/icon_close.png");
+              background-size: 100%;
+            }
+            .icon_open {
+              background: url("~assets/images/join/icon_open.png");
+              background-size: 100%;
+            }
+          }
+          &:last-child {
+            .departmentItemTitle {
+              border: 0px;
+            }
+          }
+          .positionList {
+            user-select: none;
+            .positionItem {
+              .positionItemTitle {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 0 20px;
+                height: 60px;
+                background: rgba(250, 250, 250, 1);
+                border-bottom: 1px solid #e5e5e5;
+                .positionItemName {
+                  font-size: 18px;
+                  &.active {
+                    color: #00A1DF;
+                  }
+                }
+                .positionItemInfo {
+                  font-size: 14px;
+                }
+              }
+              .detail {
+                background: #ffffff;
+                padding: 0 20px;
+                border-bottom: 1px solid #e5e5e5;
+                font-size:14px;
+                line-height:20px;
+                padding-bottom: 23px;
+                h3 {
+                  font-weight: 500;
+                  padding: 20px 0 10px 0;
+                  user-select: text;
+                }
+                span {
+                  display: block;
+                  user-select: text;
+                }
+                a {
+                  display: block;
+                  width:140px;
+                  height:50px;
+                  background:rgba(255,255,255,1);
+                  border-radius:8px;
+                  border: 2px solid rgba(0,161,223,1);
+                  margin-top: 19px;
+                  font-size:18px;
+                  color:rgba(0,161,223,1);
+                  line-height:50px;
+                  text-align: center;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 @media screen and (max-width: 750px) {
   .main {
-    width: 100%;
     padding-top: 1.2rem;
     .con {
+      padding-bottom: 0;
       background: #ffffff;
       .titleBox {
-        width:7.5rem;
         height:1.8rem;
         background:url("~assets/images/join/titlePic.png");
         background-size: 100%;
         h1 {
           font-size: 0.42rem;
-          font-weight:400;
-          color:rgba(51,51,51,1);
           line-height: 1.8rem;
-          text-align: center;
-          margin-bottom: 0.2rem;
+        }
+        img {
+          display: none;
         }
       }
       .conWrap {
+        margin: 0;
+        padding: 0;
+        background: none;
+        box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.05);
         .departmentList {
           .departmentItem {
-            color: rgba(51, 51, 51, 1);
             .departmentItemTitle {
               padding: 0 0.4rem;
               height: 1rem;
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              border-bottom: 1px solid #e5e5e5;
               .departmentItemName {
                 font-size:0.28rem;
                 font-weight: 600;
@@ -480,38 +592,27 @@ export default {
               .icon_close {
                 width: 0.36rem;
                 height: 0.36rem;
-                background: url("~assets/images/join/icon_close.png");
-                background-size: 100%;
               }
-              .icon_open {
-                background: url("~assets/images/join/icon_open.png");
-                background-size: 100%;
+            }
+            &:last-child {
+              .departmentItemTitle {
+                border-bottom: 1px solid #e5e5e5;
               }
             }
             .positionList {
               .positionItem {
                 .positionItemTitle {
-                  display: flex;
-                  justify-content: space-between;
-                  align-items: center;
                   padding: 0 0.4rem;
                   height: 1rem;
-                  background: rgba(250, 250, 250, 1);
-                  border-bottom: 1px solid #e5e5e5;
                   .positionItemName {
                     font-size:0.3rem;
-                    &.active {
-                      color: #00A1DF;
-                    }
                   }
                   .positionItemInfo {
                     font-size:0.24rem;
                   }
                 }
                 .detail {
-                  background: #ffffff;
                   padding: 0 0.4rem;
-                  border-bottom: 1px solid #e5e5e5;
                   font-size:0.26rem;
                   line-height:0.37rem;
                   padding-bottom: 0.5rem;
@@ -519,21 +620,13 @@ export default {
                     font-weight: 500;
                     padding: 0.3rem 0 0.1rem 0;
                   }
-                  span {
-                    display: block;
-                  }
                   a {
-                    display: block;
                     width:2.4rem;
                     height:0.8rem;
-                    background:rgba(255,255,255,1);
                     border-radius:0.08rem;
-                    border: 0.02rem solid rgba(0,161,223,1);
                     margin-top: 0.5rem;
                     font-size:0.3rem;
-                    color:rgba(0,161,223,1);
                     line-height:0.8rem;
-                    text-align: center;
                   }
                 }
               }
