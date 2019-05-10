a390f05da92c7f2f8936f3a4e296872ff4921666
dengping <ping.deng@kuwo.cn>
2019-05-10 14:33:29 +0800

    fix:数据更改

--- src/pages/index/components/third.vue
+++ src/pages/index/components/third.vue
@@ -5,7 +5,7 @@
       <div class="yf02-s-box" :class="{animated: circleRoate}">
         <div class="yf02-s animated delay-2s" :class="{'slideInRight': circleRoate}"></div>
       </div>
-      <h3 class="animated" :class="{fadeIn: circleRoate}">不一样的主播</h3>
+      <h3 class="animated" :class="{fadeIn: circleRoate}">不一样的主播|剧中角色|老师</h3>
     </div>
     <div class="border-big-w"></div>
     <div class="border-big">
@@ -22,8 +22,8 @@
         <div class="yf-x animated delay-1s" :class="{slideInLeft: circleRoate}"></div>
       </div>
       <div class="con">
-        <h3 class="animated" :class="{fadeIn: circleRoate}">不一样的主播</h3>
-        <span class="desc animated delay-1s" :class="{fadeIn: circleRoate}">欧美成人与双语儿童主播，无需拥有英语基础，从零开始的孩子无障碍开启</span>
+        <h3 class="animated" :class="{fadeIn: circleRoate}">不一样的主播|剧中角色|老师</h3>
+        <span class="desc animated delay-1s" :class="{fadeIn: circleRoate}">纯正欧美成人与华裔儿童双语主播组合<br/>真实展现生活场景中对话+音频剧的创新节目<br/>国内第1个[原创]的英文对话音频节目 + 孩童成长的音频家庭情景剧</span>
         <div class="anchor animated delay-2s" :class="{fadeIn: circleRoate}">
           <div class="anchorList" :class="`pos${cur}`"  @mouseenter="enter" @mouseleave="leave">
             <div class="anchorItem" v-for="(item, index) in anchorList" :key="index">
@@ -85,18 +85,38 @@ export default {
     return {
       anchorList: [
         {
-          name: 'NOORLINAH MOHAMED',
-          info: 'Noorlinah是一名屡获殊荣的女演员、教学艺术家和新加坡公共参与艺术项目的创造者，自1998年以来，她与新加坡多家戏剧公司以及亚洲、欧洲和美国进行过演出。她拥有华威大学艺术教育学博士学位，在学校担任实习工作，为艺术教育学领域的艺术家和教师开发课程和专业发展。此外，Noorlinah是国家艺术委员会艺术课程教学的首席实践者和顾问，也是O.P.E.的主任这是新加坡国际艺术节的创意展前活动。',
+          name: 'Jojo',
+          info: 'She is a curious little girl in our drama. She is four years old. Listeners follow her adventures as she learns about life’s important lesson and values her parents pass on to her. She sometimes makes mistakes, but is always willing to learn. “Never fear mistakes. You can always learn from mistakes,” her mother often says to her. “Keep trying and keep learning.”她是我们音频情景剧中的主人公，她是一个好奇的小女孩。 她今年四岁啦。 听众们跟随她的成长经历，来了解到她的父母如何传递给孩子重要的教育引导和价值观。Jojo有时会犯错误，但总是愿意学做很多事情，改正自己的错误。 “永远不要害怕错误，你总能从错误中吸取教训，“她的母亲经常对她说。 “继续努力并继续多学学吧。”',
           pic: require('../../../assets/images/tx01.jpg')
         },
         {
-          name: 'NOORLINAH MOHAMED11111',
-          info: 'Noorlinah是一名屡获殊荣的女演员、教学艺术家和新加坡公共参与艺术项目的创造者，自1998年以来，她与新加坡多家戏剧公司以及亚洲、欧洲和美国进行过演出。她拥有华威大学艺术教育学博士学位，在学校担任实习工作，为艺术教育学领域的艺术家和教师开发课程和专业发展。此外，Noorlinah是国家艺术委员会艺术课程教学的首席实践者和顾问，也是O.P.E.的主任这是新加坡国际艺术节的创意展前活动。',
+          name: 'Jill',
+          info: 'Jill is Jojo’s mother. Jojo calls her Mummy. She works as a florist and loves arranging flowers. She is a good cook. Jojo loves her food. She is patient and kind. But sometimes, she can become angry when Jojo is naughty.Jill是Jojo的母亲。 Jojo称呼她为妈妈。 她是一名花匠，喜欢插花。 她也是个好厨师，Jojo喜欢她做的各种食物。 Jill很有耐心、非常善良，但有时候，当Jojo顽皮时，她当然也会生气。',
           pic: require('../../../assets/images/tx02.jpg')
         },
         {
-          name: 'NOORLINAH MOHAMED22222',
-          info: 'Noorlinah是一名屡获殊荣的女演员、教学艺术家和新加坡公共参与艺术项目的创造者，自1998年以来，她与新加坡多家戏剧公司以及亚洲、欧洲和美国进行过演出。她拥有华威大学艺术教育学博士学位，在学校担任实习工作，为艺术教育学领域的艺术家和教师开发课程和专业发展。此外，Noorlinah是国家艺术委员会艺术课程教学的首席实践者和顾问，也是O.P.E.的主任这是新加坡国际艺术节的创意展前活动。',
+          name: 'Tom',
+          info: 'Tom is Jojo’s father. Jojo calls him Daddy. He works as an engineer in an Aeroplane manufacturing company. He brings back toy planes or helps Jojo make paper aeroplane. He is funny, loves to laugh, and is sometimes forgetful.Tom是Jojo的父亲。 Jojo称呼他为爸爸。 他在一家飞机制造公司担任工程师。 他带回玩具飞机，或者帮助Jojo叠纸飞机。 Tom很有趣，喜欢笑，有时也很健忘。',
+          pic: require('../../../assets/images/tx03.jpg')
+        },
+        {
+          name: 'Noorlinah Mohamed',
+          info: 'An award-winning actress, teaching artist and creator of public engagement arts projects in Singapore, Noorlinah has performed with various Singapore theatre companies, as well as in Asia, Europe and the US since 1998. With a PhD in Arts Education from the University of Warwick she undertakes residencies in schools and develops curriculum and professional development for teaching artists and teachers in arts pedagogy. In addition, Noorlinah is  the Lead Practitioner and Consultant for the National Art Council’s Teaching Through the Arts Programme and the Director of  The O.P.E.N, a pre-festival of ideas of Singapore International Festival of Arts.作为新加坡屡获殊荣的女演员，教学艺术家和公共参与艺术项目的创造者，Noorlinah自1998年以来一直与多家新加坡戏剧公司以及亚洲、欧洲和美国合作。她拥有华威大学的艺术教育博士学位，在学校任职，并为艺术教育学的教学艺术家和教师开发课程和专业发展。此外，Noorlinah还是国家艺术委员会通过艺术项目教学和O.P.E.N主任的首席执业者和顾问，这是新加坡国际艺术节的预热创意活动。',
+          pic: require('../../../assets/images/tx03.jpg')
+        },
+        {
+          name: 'Rosemary Mcgowan',
+          info: 'Rosie has been creating theatre with students for over 10 years, using theatre as a tool for expression and dialogue. She is the co-director of Theatre: Connect, an international, cross-cultural youth theatre that creates relevant and issue-based work with, by and for young people. She has taught drama at numerous schools and has run projects both in Singapore and in London. Rosie is currently a full-time addiction counsellor and a theatre practitioner, facilitator and educator.罗西与学生共同创作了超过10年的戏剧，将戏剧作为表达和对话的工具。她是Theatre：Connect的联合主任，这是一个国际化的跨文化青年剧场，为年轻人创造相关的，基于问题的工作。她曾在众多学校教授戏剧，并在新加坡和伦敦开展过同类课程与项目。 Rosie目前是一名全职顾问和戏剧从业者、推动者和教育者。',
+          pic: require('../../../assets/images/tx03.jpg')
+        },
+        {
+          name: 'Tatiana Krasheninnikova',
+          info: 'Tanya is a graduate of the Vienna Music and Arts University in music education and is an accomplished concert musician playing piano and clarinet. She also has a Master’s Degree in Linguistics and obtained Distinctions in Acting and Musical Theatre Grade 8 from London Academy of Music and Dramatic Arts. Tanya was most recently teaching musical theatre, music and dance to children in Singapore at Kids Performing Academy of the Arts.Tanya毕业于维也纳音乐艺术大学音乐教育专业，是一位演奏钢琴和单簧管的音乐会音乐家。她还拥有语言学硕士学位，并获得伦敦音乐与戏剧艺术学院8级表演和音乐剧的优异成绩。 Tanya最近在新加坡儿童表演艺术学院为儿童教授音乐剧、音乐和舞蹈。',
+          pic: require('../../../assets/images/tx03.jpg')
+        },
+        {
+          name: 'Justin Lee',
+          info: 'Since moving to Singapore from England in 1994, Justin has served as an instructor for enrichment companies, focusing on areas such as drama, public speaking and creative writing at local primary and secondary schools. In addition, he has been a drama instructor at several international schools and has lectured at LASALLE College of the Arts and NAFA, amongst others.自1994年从英国搬到新加坡以来，Justin一直担任？公司的讲师，专注于当地中小学的戏剧、公共演讲和创意写作等领域。此外，他还曾在多所国际学校担任戏剧讲师，并在LASALLE艺术学院和NAFA等讲学。',
           pic: require('../../../assets/images/tx03.jpg')
         }
       ],
@@ -276,7 +296,7 @@ export default {
   .conBox {
     display: none;
     width:1300px;
-    height:640px;
+    padding-bottom: 45px;
     background:rgba(168,193,0,1);
     position: relative;
     overflow: hidden;
@@ -339,7 +359,7 @@ export default {
       opacity: 0;
       .anchorList {
         margin-top: 29px;
-        width: 1548px;
+        width: 3612px;
         display: flex;
         transition: transform 0.5s ease-in-out;
         &.pos0 {
@@ -350,6 +370,18 @@ export default {
         }
         &.pos2 {
           transform: translateX(-1032px);
+        }
+        &.pos3 {
+          transform: translateX(-1548px);
+        }
+        &.pos4 {
+          transform: translateX(-2064px);
+        }
+        &.pos5 {
+          transform: translateX(-2580px);
+        }
+        &.pos6 {
+          transform: translateX(-3096px);
         }
         .anchorItem {
           width: 516px;
@@ -378,9 +410,8 @@ export default {
         }
       }
       .btnList {
-        width: 106px;
         display: flex;
-        justify-content: center;
+        justify-content: flex-start;
         margin-top: 69px;
         span {
           width: 8px;
@@ -538,6 +569,7 @@ export default {
       display: block;
       background: none;
       overflow: inherit;
+      padding-bottom: 0;
       &.pc {
         display: none;
       }
@@ -551,7 +583,7 @@ export default {
         margin: 0 auto;
         .anchorList {
           margin-top: 0.6rem;
-          width: 17.7rem;
+          width: 41.3rem;
           &.pos0 {
             transform: translateX(0)
           }
@@ -560,6 +592,18 @@ export default {
           }
           &.pos2 {
             transform: translateX(-11.8rem);
+          }
+          &.pos3 {
+            transform: translateX(-17.7rem);
+          }
+          &.pos4 {
+            transform: translateX(-23.6rem);
+          }
+          &.pos5 {
+            transform: translateX(-29.5rem);
+          }
+          &.pos6 {
+            transform: translateX(-35.4rem);
           }
           .anchorItem {
             display: flex;
